f9bcdb44f31ecb07a968cae042f80357b9e7b3ec
Vic Chimenti <victor.chimenti@gmail.com>
2025-04-02 14:42:32 -0700

    refactor(_app) file overview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,12 @@
+/**
+ * @fileoverview Main Next.js app
+ * 
+ * This is the main entry point for the Next.js application.
+ *
+ * @author Victor Chimenti
+ * @version 1.0.0
+ */
+
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
