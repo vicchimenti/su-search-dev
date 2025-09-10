5e20d8c24c54e569bfbee629b6e14034f9bbd781
Vic Chimenti <victor.chimenti@gmail.com>
2025-09-10 09:12:32 -0700

    fix(next config) : Add cache headers to CORS config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://www.seattleu.edu" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin" }
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin, X-Cache-Only" },
+          { key: "Access-Control-Expose-Headers", value: "X-Cache-Status, X-Cache-Type, X-Cache-Tab-ID, X-Client-IP-Source" }
         ]
       }
     ];
